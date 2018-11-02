e4d85fc71c15176133b57d671f1ad25c8447beca
Bogdan Ivanyuk <35574157+BananMango@users.noreply.github.com>
2018-11-02 12:28:21 +0200

    Stack implementation using Python list
    Stack is a linear data structure which the order LIFO(Last In First Out) or FILO(First In Last Out) for accessing elements. Basic operations of stack are : Push, Pop , Peek

---
+++ Stack.py
@@ -0,0 +1,33 @@
+"A stack is a collection of objects that are inserted and removed according to the last-in, first-out (LIFO) principle."
+
+class Stack:
+    "Array stack implementation"
+    def __init__(self):
+        self._data = []
+
+    def __len__(self):
+        return len(self._data)
+
+    def is_empty(self):
+        "checks if stack is empty"
+        return len(self._data) == 0
+
+    def push(self, a):
+        "adds element to the end "
+        self._data.append(a)
+
+    def peek(self):
+        "returns element at the end"
+        if self.is_empty():
+            raise Exception('Stack is empty')
+        return self._data[-1]
+
+    def pop(self):
+        "deleted element at the end"
+        if self.is_empty():
+            raise Exception('Stack is empty')
+        return self._data.pop()
+
+
+
+
