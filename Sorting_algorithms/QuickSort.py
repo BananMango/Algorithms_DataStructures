8b425bf223b953e29f83ba197936499d7f72be8d
Bogdan Ivanyuk <35574157+KyloRen1@users.noreply.github.com>
2019-08-09 17:42:50 +0300

    Update QuickSort.py

--- Sorting_algorithms/QuickSort.py
+++ Sorting_algorithms/QuickSort.py
@@ -1,23 +1,24 @@
 def partition(array, low, high):
-	i = low - 1
-	pivot = array[high]
+    pivot = low
+    for i in range(low+1, high+1):
+        if array[i] <= array[low]:
+            pivot += 1
+            array[i], array[pivot] = array[pivot], array[i]
+    array[pivot], array[low] = array[low], array[pivot]
+    return pivot
 
-	for j in range(low, high):
-		if array[j] <= pivot:
-			i += 1
-			array[i], array[j] = array[j], array[i]
-		array[i + 1], array[high] = array[high], array[i + 1]
-	return (i + 1)
+def quick_sort(array, low=0, high=None):
+    if high is None:
+        high = len(array) - 1
+    
+    def _quick_sort(array, low, high):
+        if low >= high:
+            return
+        pivot = partition(array, low, high)
+        _quick_sort(array, low, pivot-1)
+        _quick_sort(array, pivot+1, high)
 
-def quick_sort(array, low = 0, high = None):
-	if high is None:
-		high = len(array) - 1
-	if low < high:
-		part = partition(array, low, high)
-
-		quick_sort(array, low, part-1)
-		quick_sort(array, part+1, high)
-	return array
+    return _quicksort(array, low, high)
 
 if __name__ == '__main__':
 	array = [54, 34, 476, 87, 23, 1, 23, 57, 978, 5, 68, 45, 876, 9, 34]
