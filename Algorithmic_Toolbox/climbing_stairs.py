7b8c7496145741f299ec561febcc694f33b95e71
Bogdan Ivanyuk <35574157+KyloRen1@users.noreply.github.com>
2020-10-16 16:37:32 +0300

    Update climbing_stairs.py

--- Algorithmic_Toolbox/climbing_stairs.py
+++ Algorithmic_Toolbox/climbing_stairs.py
@@ -7,6 +7,15 @@ def climbStairs(n):
         curr = next_
     return curr
     
+def climbingStairs_fast(n):
+    ''' Using Fibonacci formula '''
+    import math
+    a = (1 + math.sqrt(5)) / 2
+    b = (1 - math.sqrt(5)) / 2
+    res = math.pow(a, n+1) - math.pow(b, n+1)
+    return int(res / math.sqrt(5))    
+
 if __name__ == '__main__':
   n = 4
   print(climbStairs(n))
+  print(climbingStairs_fast(n))
