726493bd436c7ebb534a63d80ecf6809428804b4
Bogdan Ivanyuk <35574157+KyloRen1@users.noreply.github.com>
2020-10-16 13:15:20 +0300

    Update fast_power.py

--- Algorithmic_Toolbox/fast_power.py
+++ Algorithmic_Toolbox/fast_power.py
@@ -6,9 +6,21 @@ def calculate_power(a, p):
         a *= a
         p >>= 1
     return res
+
+def power_simple(a, n):
+    res = 1
+    while (n != 0):
+        if (n % 2 == 1):
+            res *= a
+        a *= a
+        n = n // 2
+    return res
+
     
 if __name__ == '__main__':
     n = 13
     power = 5
     res = calculate_power(n, power)
+    print(res)
+    res = power_simple(n, power)
     print(res)
