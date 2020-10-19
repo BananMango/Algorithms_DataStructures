b848d2ac5ac9d05ea2ca4eae2a9cbe796c5b9d70
Bogdan Ivanyuk <35574157+KyloRen1@users.noreply.github.com>
2020-10-19 22:20:38 +0300

    fixed

--- Algorithmic_Toolbox/binary_search.py
+++ Algorithmic_Toolbox/binary_search.py
@@ -1,19 +1,16 @@
 
 def binary_search(arr, val):
-	l = 0
-	r = len(arr)
-	mid = 0
-
-	while (l+1 < r):
-		mid = l + (r - l) // 2
-		if val > arr[mid]:
-			l = mid
-		else:
-			r = mid
-	if arr[mid] == val:
-		return mid
-	else:
-		return -1
+    l = 0
+    r = len(nums)
+    while l + 1 < r:
+        m = l + (r - l) // 2
+        if nums[m] > target:
+            r = m
+        else:
+            l = m
+    if nums[l] == target:
+        return l
+    else: return -1
 
 if __name__ == '__main__':
 	array = [1, 2, 5, 7, 13, 15, 16, 18, 24, 28, 29]  
