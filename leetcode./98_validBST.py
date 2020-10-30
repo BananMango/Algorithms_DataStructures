9ce233ed460224d6cd109d83e4518bf51c694144
Bogdan Ivanyuk <35574157+KyloRen1@users.noreply.github.com>
2020-10-30 15:52:27 +0200

    Update 98_validBST.py

--- leetcode./98_validBST.py
+++ leetcode./98_validBST.py
@@ -12,3 +12,21 @@ class Solution:
     
     def isValidBST(self, root: TreeNode) -> bool:
         return self.isValidBSTRecursive(root, None, None)
+
+    
+    
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        def helper(root, lower = float('-inf'), upper = float('inf')):
+            if not root:
+                return True
+            val = root.val
+            if val <= lower or val >= upper:
+                return False
+            
+            if not helper(root.right, val, upper):
+                return False
+            if not helper(root.left, lower, val):
+                return False
+            return True
+        return helper(root)
