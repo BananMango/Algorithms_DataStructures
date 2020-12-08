44871ce137bd49c2c3a730349f027496cd55c237
KyloRen1 <ivanyuk.bogdan1999@yahoo.com>
2020-12-08 19:05:25 +0200

    c done

--- codeforces/round686/c_sequence_transformation.py
+++ codeforces/round686/c_sequence_transformation.py
@@ -1,32 +1,37 @@
 
-def remove_duplicates(arr):
-	res = [arr[0]]
-	for i in range(1, len(arr)):
-		if arr[i] != res[-1]:
-			res.append(arr[i])
-	return res
+def unique(arr):
+    res = [arr[0]]
+    for i in range(1, len(arr)):
+        if arr[i] != res[-1]:
+            res.append(arr[i])
+    return res
+
 
 def n_operations(arr):
-	cleaned = remove_duplicates(arr)
+    cleaned = unique(arr)
 
-	counter = [1] * (len(cleaned) + 1)
-	for i in range(len(cleaned)):
-		counter[cleaned[i]] += 1
-	counter[cleaned[0]] -= 1
-	counter[cleaned[-1]] -= 1
+    occurences = {}
+    for el in cleaned:
+        if el in occurences.keys():
+            occurences[el] += 1
+        else:
+            occurences[el] = 1
 
-	res = float('inf')
-	for i in range(len(cleaned)):
-		res = min(res, counter[cleaned[i]])
-	return res
+    best = len(cleaned)
+    for el in cleaned:
+        best = min(best, occurences[el] + 1 -
+                   (cleaned[0] == el) - (cleaned[-1] == el))
+    return best
+
 
 def main():
-	t = int(input())
-	for i in range(t):
-		n = int(input())
-		arr = list(map(int, input().split(' ')))
-		res = n_operations(arr)
-		print(res)
-	
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        arr = list(map(int, input().split(' ')))
+        res = n_operations(arr)
+        print(res)
+
+
 if __name__ == '__main__':
-	main()
+    main()
